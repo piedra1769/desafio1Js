ebfa15472a1ca82a44376173309e07cfb338805f
Sebastian Piedrahita <sebastianpiedrahitao@gmail.com>
2022-02-23 23:46:36 -0500

    Modificaciones de estilo

--- desafio1.js
+++ desafio1.js
@@ -1,4 +1,4 @@
-alert('Tenemos las siguientes opciones para tu viaje');
+alert('Tenemos las siguientes opciones para tu viaje. \n Selecciona y te daremos el precio del paquete');
 
 // Se podrá seleccionar entre dos opciones diferentes: Santa Marta o Cartagena
 
@@ -6,24 +6,24 @@ let destino = parseInt(prompt('Selecciona tu destino: \n 1. Santa Marta \n 2. Ca
 
 // Restringir las elecciones a los números 1 o 2
 while (destino < 1 || destino > 2 || isNaN(destino)){
-alert('Debes seleccionar solo una opción entre 1 y 2');
-opcion = parseint(prompt('Selecciona tu destino: \n 1. Santa Marta \n 2. Cartagena'));
+alert('Esta no es una opción, presiona F5 y selecciona 1 o 2');
+break;
 }
 
-// Descripción del primera opción
+// Descripción del primera opciones
 switch (destino) {
     
     case 1:
     const precioSantaMarta = 500000
     let personasSM = parseInt (prompt('¿Cuántas personas viajan'));
     const cotizacionSm = precioSantaMarta * personasSM;
-    alert (cotizacionSm);
+    alert (cotizacionSm + ' COP');
     break;
         
     case 2:
     const precioCartagena = 400000
     let personasCT = parseInt (prompt('¿Cuántas personas viajan'));
     const cotizacionCt = precioCartagena * personasCT;
-    alert (cotizacionCt);
+    alert (cotizacionCt + ' COP');
     break;
 }
